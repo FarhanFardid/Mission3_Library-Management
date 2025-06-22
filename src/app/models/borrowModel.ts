70687b57aa1b2eb2e03b5f986a4657a6b7ae6621
FarhanFardid <farhanfardid009@gmail.com>
2025-06-22 22:21:16 +0600

    borrow intrface and models implemeted

---
+++ src/app/models/borrowModel.ts
@@ -0,0 +1,30 @@
+import { model, Schema } from "mongoose";
+import { IBorrow } from "../interfaces/borrowInterface";
+
+const borrowSchema = new Schema<IBorrow>(
+  {
+    book: {
+      type: Schema.Types.ObjectId,
+      ref: 'Book',
+      required: [true, 'Book ID is required for borrowing'],
+    },
+    quantity: {
+      type: Number,
+      required: [true, 'Quantity is required'],
+      min: [1, 'Quantity must be at least 1'],
+      validate: {
+        validator: Number.isInteger,
+        message: 'Quantity must be an integer',
+      },
+    },
+    dueDate: {
+      type: Date,
+      required: [true, 'Due date is required'],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Borrow = model<IBorrow>('Borrow', borrowSchema);
